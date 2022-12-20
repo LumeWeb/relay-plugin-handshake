cf095ce202554e857e14fd8b7c7c85717cc673d7
Derrick Hammer <derrick@derrickhammer.com>
2022-12-20 10:46:48 -0500

    *Remove unneeded build files

--- build.js
+++
@@ -1,9 +0,0 @@
-import esbuild from 'esbuild'
-
-esbuild.buildSync({
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/handshake.js',
-    format: 'cjs',
-    bundle: true,
-    platform: "node"
-})
